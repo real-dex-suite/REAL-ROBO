aa4d1092532de58db8863f48b757afc8036b49ae
thwu <11071192+thwu17@user.noreply.gitee.com>
2024-01-19 16:21:17 +0800

    vec ops update

--- holodex/utils/vec_ops.py
+++ holodex/utils/vec_ops.py
@@ -39,3 +39,46 @@ def calculate_angle(coord_1, coord_2, coord_3):
 
 def coord_in_bound(bound, coord):
     return cv2.pointPolygonTest(np.float32(bound), np.float32(coord), False)
+
+def best_fit_transform(A, B):
+        """
+        Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+        Input:
+        A: Nxm numpy array of corresponding points
+        B: Nxm numpy array of corresponding points
+        Returns:
+        T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+        R: mxm rotation matrix
+        t: mx1 translation vector
+        """
+
+        assert A.shape == B.shape
+
+        # get number of dimensions
+        m = A.shape[1]
+
+        # translate points to their centroids
+        centroid_A = np.mean(A, axis=0)
+        centroid_B = np.mean(B, axis=0)
+        AA = A - centroid_A
+        BB = B - centroid_B
+
+        # rotation matrix
+        H = np.dot(AA.T, BB)
+        U, S, Vt = np.linalg.svd(H)
+        R = np.dot(Vt.T, U.T)
+
+        # special reflection case
+        if np.linalg.det(R) < 0:
+            Vt[m - 1, :] *= -1
+            R = np.dot(Vt.T, U.T)
+
+        # translation
+        t = centroid_B.T - np.dot(R, centroid_A.T)
+
+        # homogeneous transformation
+        T = np.identity(m + 1)
+        T[:m, :m] = R
+        T[:m, m] = t
+
+        return T, R, t
