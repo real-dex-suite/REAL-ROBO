eaf1c4d40347ed3097a456f1fa99a13bd6f95990
thwu <11071192+thwu17@user.noreply.gitee.com>
2024-02-05 16:18:45 +0800

    dummy data recorder

--- holodex/data/collector.py
+++ holodex/data/collector.py
@@ -5,19 +5,33 @@ import time
 from sensor_msgs.msg import JointState
 from holodex.utils.files import *
 from holodex.constants import *
-from holodex.robot import AllegroHand
-from holodex.utils.network import ImageSubscriber, frequency_timer
+from holodex.utils.network import ImageSubscriber, frequency_timer, Float64MultiArray, TactileSubscriber
 
+# load module according to hand type
+hand_module = __import__("holodex.robot.hand")
+Hand_module_name = f'{HAND_TYPE}Hand'
+Hand = getattr(hand_module.robot, Hand_module_name)
 
 class DataCollector(object):
     def __init__(
         self,
+        num_tactiles,
         num_cams,
         storage_path
     ):
         rospy.init_node('data_extractor', disable_signals = True)
 
         self.storage_path = storage_path
+
+        # ROS Subscribers based on the number of tactile board used
+        self.num_tactiles = num_tactiles
+        self.tactile_subscribers = []
+        for tactile_num in range(self.num_tactiles):
+            self.tactile_subscribers.append(
+                TactileSubscriber(
+                    tactile_num = tactile_num + 1
+                )
+            )
 
         # ROS Subscribers based on the number of cameras used
         self.num_cams = num_cams
@@ -38,34 +52,50 @@ class DataCollector(object):
                 )
             )
 
-        # Allegro Hand controller initialization
-        self.allegro_hand = AllegroHand()
+        # Hand controller initialization
+        self.hand = Hand()
 
         # ROS Subscriber to get the arm information
-        self.kinova_joint_state = None
-        rospy.Subscriber(KINOVA_JOINT_STATE_TOPIC, JointState, self._callback_kinova_joint_state, queue_size = 1)
+        self.arm_joint_state = None
+        self.arm_ee_pose = None
+        rospy.Subscriber(JAKA_JOINT_STATE_TOPIC, JointState, self._callback_arm_joint_state, queue_size = 1)
+        rospy.Subscriber(JAKA_EE_POSE_TOPIC, Float64MultiArray, self._callback_arm_ee_pose, queue_size = 1)
 
         self.frequency_timer = frequency_timer(RECORD_FPS)
 
-    def _callback_kinova_joint_state(self, data):
-        self.kinova_joint_state = data
+    def _callback_arm_joint_state(self, data):
+        self.arm_joint_state = data
+    
+    def _callback_arm_ee_pose(self, data):
+        self.arm_ee_pose = data
 
     def extract(self, offset = 0):
         counter = offset + 1
         try:
             while True:
                 skip_loop = False
+
                 # Checking for broken data streams
-                if self.allegro_hand.get_hand_position() is None or self.kinova_joint_state is None:
+                for tactile_subscriber in self.tactile_subscribers:
+                    if tactile_subscriber.get_data() is None:
+                        print('Tactile data not available!')
+                        skip_loop = True
+
+                if self.hand.get_hand_position() is None:
+                    print('Hand data not available!')
+                    skip_loop = True
+                if self.arm_joint_state is None:
+                    print('Arm data not available!')
                     skip_loop = True
 
                 for color_image_subscriber in self.color_image_subscribers:
                     if color_image_subscriber.get_image() is None:
+                        print('Color image not available!')
                         skip_loop = True
 
-                for depth_image_subscriber in self.depth_image_subscribers:
-                    if depth_image_subscriber.get_image() is None:
-                        skip_loop = True
+                # for depth_image_subscriber in self.depth_image_subscribers:
+                #     if depth_image_subscriber.get_image() is None:
+                #         skip_loop = True
 
                 if skip_loop:
                     continue
@@ -74,16 +104,17 @@ class DataCollector(object):
 
                 for cam_num in range(self.num_cams):
                     state['camera_{}_color_image'.format(cam_num + 1)] = self.color_image_subscribers[cam_num].get_image()
-                    state['camera_{}_depth_image'.format(cam_num + 1)] = self.depth_image_subscribers[cam_num].get_image()
+                    # state['camera_{}_depth_image'.format(cam_num + 1)] = self.depth_image_subscribers[cam_num].get_image()
                 
-                # Allegro data
-                state['allegro_joint_positions'] = self.allegro_hand.get_hand_position()
-                state['allegro_joint_velocity'] = self.allegro_hand.get_hand_velocity()
-                state['allegro_joint_effort'] = self.allegro_hand.get_hand_torque()
-                state['allegro_commanded_joint_position'] = self.allegro_hand.get_commanded_joint_position()
+                # Hand data
+                state['hand_joint_positions'] = self.hand.get_hand_position()
+                state['hand_joint_velocity'] = self.hand.get_hand_velocity()
+                state['hand_joint_effort'] = self.hand.get_hand_torque()
+                state['hand_commanded_joint_position'] = self.hand.get_commanded_joint_position()
 
                 # Kinova data
-                state['kinova_joint_positions'] = self.kinova_joint_state.position
+                state['arm_joint_positions'] = self.arm_joint_state.position
+                state['arm_ee_pose'] = self.arm_ee_pose.data
 
                 # Temporal information
                 state['time'] = time.time()
--- record_data.py
+++ record_data.py
@@ -8,7 +8,7 @@ def main(configs):
     demo_path = os.path.join(os.getcwd(), configs['storage_path'], f'demonstration_{configs.demo_num}')
     make_dir(demo_path)
 
-    collector = DataCollector(num_cams = configs['num_cams'], storage_path = demo_path)
+    collector = DataCollector(num_tactiles= configs['num_tactiles'] ,num_cams = configs['num_cams'], storage_path = demo_path)
 
     print(f'Recording data for demonstration: {configs.demo_num} with offset: {configs.offset}.')
     print(f'Storing the demonstration data in {demo_path}')
