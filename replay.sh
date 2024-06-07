b2f350515f6b414ca061bf093fc777af2072df1f
kingchou007 <jl3485@cornell.edu>
2024-06-07 14:41:16 +0800

    update reaply script

---
+++ replay.sh
@@ -0,0 +1,7 @@
+python /home/agibot/Projects/Real-Robo/replay.py \
+    data_path="expert_dataset/openbottle_filtered_img/recorded_data/demonstration_1" \
+    extract_path="expert_dataset/openbottle_filtered_img/extracted_data/filtered/actions/demonstration_1.pth" \
+    extract_replay_images=true \
+    image_size=224\
+    num_cams=1 \
+
