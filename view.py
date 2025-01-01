dfab664c0f44d73165eaddcb21384d5be06d58c6
kingchou007 <jl3485@cornell.edu>
2025-01-01 18:28:48 +0800

    clean

--- view.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-data_path = '/home/agibot/Projects/Real-Robo/expert_dataset/pouring-v1/recorded_data/demonstration_8/2'
-
-# Load the data
-data = np.load(data_path, allow_pickle=True)
-print(data)
