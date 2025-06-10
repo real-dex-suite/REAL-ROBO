c8c2b634494c1e40de438b7d9f42e4681b50e1a5
fanhongwei <fanhongwei@zhiyuan-robot.com>
2025-06-10 10:16:21 +0800

    teleop

--- tools/teleoperation/teleop.py
+++ tools/teleoperation/teleop.py
@@ -1,23 +1,24 @@
 import hydra
 import time
-from holodex.processes import get_camera_stream_processes, get_detector_processes, get_teleop_process
+from holodex.processes import get_tactile_stream_processes, get_detector_processes, get_teleop_process, get_tactile_visualizer_process
 import multiprocessing
 
 @hydra.main(version_base = '1.2', config_path='../../configs', config_name='teleop_franka_pico')
 def main(configs):    
-    # Obtaining all the robot streams
     multiprocessing.set_start_method('spawn')
     
-    robot_camera_processes, robot_camera_stream_processes = get_camera_stream_processes(configs)
+    # Obtaining all the robot streams
+    tactile_processes = get_tactile_stream_processes(configs)
     detection_process, keypoint_transform_processes, plotter_processes = get_detector_processes(configs)
     teleop_process = get_teleop_process(configs)
+    tactile_visualizer_process = get_tactile_visualizer_process(configs)
 
-    for process in robot_camera_processes:
+    # Starting all the processes
+    for process in tactile_processes:
         process.start()
         time.sleep(2)
 
-    if configs.tracker.type != 'HAMER': # only for temporal
-        # # Detection processes
+    if detection_process is not None:
         detection_process.start()
 
     for process in keypoint_transform_processes:
@@ -26,24 +27,18 @@ def main(configs):
     for process in plotter_processes:
         process.start()
     
-
-    # tactile_visualizer_process.start()
+    if tactile_visualizer_process is not None:
+        tactile_visualizer_process.start()
 
     time.sleep(2)
     # Teleop process
     teleop_process.start()
 
     # Joining all the processes
-    # for process in tactile_processes:
-    #     process.join()
-
-    for process in robot_camera_processes:
+    for process in tactile_processes:
         process.join()
 
-    for process in robot_camera_stream_processes:
-        process.join()
-
-    if configs.tracker.type != 'HAMER': # only for temporal
+    if detection_process is not None:
         detection_process.join()
 
     for process in keypoint_transform_processes:
@@ -51,8 +46,10 @@ def main(configs):
 
     for process in plotter_processes:
         process.join()
-
-    # tactile_visualizer_process.join()
+        
+    if tactile_visualizer_process is not None:
+        tactile_visualizer_process.join()
+        
     teleop_process.join()
 
 
