dcf498e423c93f134a756f41623bf87eec051a95
hwfan <hwfan_bupt@outlook.com>
2025-06-12 02:26:11 +0800

    reset

--- tools/teleoperation/reset_franka.py
+++ tools/teleoperation/reset_franka.py
@@ -1,8 +1,8 @@
 import argparse
 from frankapy import FrankaArm
 
+DEFAULT_MOBILE_FRANKA = [-0.00950185, -0.11585174, -0.09009125, -2.25216384, -0.0030539, 2.14814552, 1.41996225]
+
 if __name__ == '__main__':
-    print('Starting robot')
-    fa = FrankaArm()
-    print("start ok")
-    fa.goto_joints([0.0405, -0.0053, -0.1853, -2.2179, -0.0057, 2.2234, 0.6904])
+    fa = FrankaArm(with_gripper=False)
+    fa.goto_joints(DEFAULT_MOBILE_FRANKA)
