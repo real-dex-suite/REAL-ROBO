8f7ea89cbb70bf6cb0eb39721e0a78fe7976a85f
kingchou007 <jl3485@cornell.edu>
2025-06-23 15:50:39 +0800

    fr3

--- tools/teleoperation/reset_franka.py
+++ tools/teleoperation/reset_franka.py
@@ -2,11 +2,8 @@ import argparse
 from frankapy import FrankaArm
 
 # isaac sim
-# DEFAULT_MOBILE_FRANKA = [-0.00950185, -0.11585174, -0.09009125, -2.25216384, -0.0030539, 2.14814552, 1.41996225]
-
-# bread
-DEFAULT_MOBILE_FRANKA = [-1.02085034, -0.15071283, 1.12373157, -2.49985287, 1.3068513, 3.24757076, 0.41839715]
+DEFAULT_FR3 = [-0.00950185, -0.11585174, -0.09009125, -2.25216384, -0.0030539, 2.14814552, 1.41996225]
 
 if __name__ == '__main__':
-    fa = FrankaArm(with_gripper=False)
-    fa.goto_joints(DEFAULT_MOBILE_FRANKA)
+    fa = FrankaArm()
+    fa.goto_joints(DEFAULT_FR3)
